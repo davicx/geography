46f69e8c452a775eab4b965768acb4749ce7a48f
David <davidcharlesv@gmail.com>
2024-05-14 15:35:56 -0700

    Added Hyperlink

--- examples/excelHandling/hyperlink.py
+++ examples/excelHandling/hyperlink.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def get_hyperlink():
     cwd = Path.cwd()
-    excel_file = cwd / 'examples' / 'excelHandling' /  'excela' / 'hyperink.xlsx'
+    excel_file = cwd / 'examples' / 'excelHandling' /  'excel' / 'hyperink.xlsx'
 
     #Open with full path
     #excelBook = openpyxl.load_workbook('/Users/david/Desktop/David/www/geography/examples/excelHandling/excel/hyperink.xlsx')
@@ -18,5 +18,6 @@ def get_hyperlink():
     return excelSheet.cell(row=2, column=1).hyperlink.target
 
 hyperlink = get_hyperlink()
-print("hyperlink")
+print("Hyperlink")
 print(hyperlink)
+print(" ")
